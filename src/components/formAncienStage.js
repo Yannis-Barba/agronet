84d6957b3ebdd475a4119981c87c8168c5e1addb
Yannis <yannis.barba.dev@gmail.com>
2021-10-18 19:43:15 +0200

    made the button form style: ajouter un stage

--- src/components/formAncienStage.js
+++ src/components/formAncienStage.js
@@ -3,15 +3,22 @@ import React from 'react';
 function FormAncienStage(){
     return (
         <div className="formContainer">
+            <h2> Préférences </h2>
             <form className='formAncienStage'>
                 <label htmlFor="lieu"/>
-                <input id="lieu" placeholder="lieu"/>
+                <input id="lieu" placeholder="Lieu"/>
                 <label htmlFor="promotion"/>
-                <input id="promotion" placeholder="promotion"/>
+                <input id="promotion" placeholder="Promotion"/>
                 <label htmlFor="type"/>
-                <input id="type" placeholder="type"/>
-
+                <input id="type" placeholder="Type"/>
+                <label htmlFor="remuneration"/>
+                <input id="remuneration" placeholder="Rémunération"/>
+                <label htmlFor="domaine"/>
+                <input id="domaine" placeholder="Domaine"/>
+                <label htmlFor="specialite"/>
+                <input id="specialite" placeholder="Spécialité"/>
             </form>
+            <button className="button ajoutStageButton">Ajouter un Stage</button>
         </div>
     )
 }
