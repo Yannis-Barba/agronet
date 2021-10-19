868c325ff7ec401b992d88c8e1e02607cdf73caa
Yannis <yannis.barba.dev@gmail.com>
2021-10-19 21:15:59 +0200

    made the simulation of the filter: an array of stage (object) an array for the list of filters and an array for the filters choosen by the user, I used the filter an map functions to draw or not the stage according to the filters

--- src/components/stage.js
+++ src/components/stage.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-function Stage(){
+function Stage({sujet, ecole, type, cursus, lieu, promotion, domaine, specialite, remuneration}){
     return (
         <div className="stage">
-            <div className="specialite"><p>Spécialité</p></div>
+            <div className="specialite"><p>{specialite}</p></div>
             <div className="stageContentContainer">
                 <div className="titles" >
-                    <h3> Sujet de Stage </h3>
-                    <h4>Type de Stage</h4>
+                    <h3> {sujet}</h3>
+                    <h4>{type}</h4>
                 </div>
                 <ul className="listInformations">
                     <li> Nom d'organisme d'accueil : </li>
                     <li> Type d'organisme d'accueil : </li>
                     <li> Date de début : </li>
                     <li> Date de Fin : </li>
-                    <li> Rémunération : </li>
+                    <li> Rémunération : {remuneration ? "OUI" : "NON"} </li>
                 </ul>
             </div>
         </div>
--- src/pages/ancienstage.js
+++ src/pages/ancienstage.js
@@ -4,20 +4,102 @@ import FormAncienStage from '../components/formAncienStage';
 import Stage from '../components/stage';
 
 function AncienStage(){
+
+    let listAncienStage = [
+        {
+            sujet: "Sujet 1",
+            type: "Mission",
+            ecole: "ACO",
+            cursus: "Agronome",
+            lieu: "France",
+            promotion: "170",
+            domaine: "entreprise",
+            specialite: "POMAR",
+            remuneration: false,
+        },
+        {
+            sujet: "Sujet 2",
+            type: "Mission",
+            ecole: "ACO",
+            cursus: "Agronome",
+            lieu: "Europe",
+            promotion: "169",
+            domaine: "entreprise",
+            specialite: "Agro-éco",
+            remuneration: false,
+        },
+        {
+            sujet: "Sujet 3",
+            type: "FLE",
+            ecole: "ACO",
+            cursus: "Agronome",
+            lieu: "Europe",
+            promotion: "170",
+            domaine: "entreprise",
+            specialite: "POMAR",
+            remuneration: false,
+        },
+        {
+            sujet: "Sujet 4",
+            type: "Mission",
+            ecole: "ACO",
+            cursus: "Agronome",
+            lieu: "France",
+            promotion: "168",
+            domaine: "entreprise",
+            specialite: "PA",
+            remuneration: false,
+        },
+        {
+            sujet: "Sujet 5",
+            type: "FLE",
+            ecole: "ACO",
+            cursus: "Agronome",
+            lieu: "France",
+            promotion: "167",
+            domaine: "recherche",
+            specialite: "BMC",
+            remuneration: true,
+        },
+    ];
+
+    let filters = ["ecole", "type", "cursus", "lieu", "promotion", "domaine", "specialite", "remuneration"];
+
+    let filter = {
+        ecole: ["ACO", "MSA", "ASD"],
+        type: ["FLE", "Mission"], 
+        cursus: ["Agronome"],
+        lieu: ["France", "Europe"],
+        promotion: ["167", "168", "169", "170"],
+        domaine: ["entreprise", "recherche"],
+        specialite: ["POMAR", "PA", "BMC"], 
+        remuneration: [true, false],
+    }
+
     return (
         <>
         <div id="ancienStage">
             <div className="formAndStage">
                 <FormAncienStage />
                 <div className="stages">
-                    <Stage/>
-                    <Stage/>
-                    <Stage/>
-                    <Stage/>
-                    <Stage/>
-                    <Stage/>
-                    <Stage/>
-                    <Stage/>
+                    {listAncienStage.filter((stage) => {
+                        console.log(stage);
+                        let show = true; 
+                        filters.forEach((f) => {
+                            console.log("filtre :",f);
+                            console.log("filter[f]", filter[f]);
+                            console.log("stage[f]", stage[f]);
+                            if(!filter[f].includes(stage[f])){
+                                show = false
+                            }
+                        })
+                        console.log("show : ", show);
+                        if(show){
+                            return stage;
+                        }  
+                    }).map((stage, index) => {
+                        return <Stage key={index} sujet={stage.sujet} type={stage.type} specialite={stage.specialite} remuneration={stage.remuneration}/>
+                    })}
                 </div>
             </div>
             <Map />
